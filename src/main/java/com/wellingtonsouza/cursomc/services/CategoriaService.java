e9d996b3e931c90b66ded0413b2716b8891bdbc9
Wellington Souza <wellingtonwns@gmail.com>
2020-02-14 16:53:02 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/wellingtonsouza/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.wellingtonsouza.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellingtonsouza.cursomc.domain.Categoria;
+import com.wellingtonsouza.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+				
+	}
+
+}
